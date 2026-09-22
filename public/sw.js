adebf053b59ebdcbfcb0ff19628f5f8e8e59e081
alekseevaleksey18061994-gif <alekseev.aleksey18061994@gmail.com>
2026-09-22 03:02:52 +0300

    Сбросить PWA-кэш v10

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE='content-factory-v9';
-const CORE=['/','/style.css?v=9','/app.js?v=9','/manifest.webmanifest?v=9'];
+const CACHE='content-factory-v10';
+const CORE=['/','/style.css?v=10','/app.js?v=10','/manifest.webmanifest?v=10'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
