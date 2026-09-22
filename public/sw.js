9772a5027919af69300ba6d4e6049aa0b155b373
alekseevaleksey18061994-gif <alekseev.aleksey18061994@gmail.com>
2026-09-22 03:28:30 +0300

    Обновить PWA-кэш для медиатеки v13

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE='content-factory-v12';
-const CORE=['/','/style.css?v=12','/app.js?v=12','/manifest.webmanifest?v=12'];
+const CACHE='content-factory-v13';
+const CORE=['/','/style.css?v=13','/app.js?v=13','/manifest.webmanifest?v=13'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
