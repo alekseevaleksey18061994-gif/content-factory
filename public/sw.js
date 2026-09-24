f827d2aef3a3e25c247c799bdc1689578e45593a
alekseevaleksey18061994-gif <alekseev.aleksey18061994@gmail.com>
2026-09-24 13:44:54 +0300

    Refresh service worker cache for v2.6.18

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const BUILD='content-factory-v41';
+const BUILD='content-factory-v42';
 self.addEventListener('install',event=>event.waitUntil(self.skipWaiting()));
 self.addEventListener('activate',event=>event.waitUntil((async()=>{
   const keys=await caches.keys();
@@ -8,7 +8,7 @@ self.addEventListener('activate',event=>event.waitUntil((async()=>{
   await Promise.all(clients.map(async client=>{
     try{
       const u=new URL(client.url);
-      u.searchParams.set('build','41');
+      u.searchParams.set('build','42');
       await client.navigate(u.toString());
     }catch{}
   }));
